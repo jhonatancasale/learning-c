37882e4a9ba21e2a534415461b3e55c8c2810c48
jhonatancasale <jhonatan@jhonatancasale.com>
2017-03-01 10:34:54 -0300

    Add third example

--- conceitos/basicos/gerais/0x06_loops/do_while/example_do_while.c
+++ conceitos/basicos/gerais/0x06_loops/do_while/example_do_while.c
@@ -14,9 +14,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int read_value(void);
+
 int main (int argc, char **argv)
 {
   int value = 0;
+
+  printf("Example: 01 \n");
 
   do {
     printf ("Choose one:\n");
@@ -24,10 +28,28 @@ int main (int argc, char **argv)
     scanf("%i", &value);
   } while ( value <= 0 || value > 10 );
 
+
+  printf("Example: 02 \n");
+
   int i = 0;
   do printf ("%i\n", i);
 
   while ( ++i < value );
 
+  printf("Example: 03 \n");
+
+  do value = read_value();
+  while ( value <= 0 || value > 10 );
+
   return (EXIT_SUCCESS);
+}
+
+int read_value(void)
+{
+  int value = 0;
+  printf ("Choose one:\n");
+  printf ("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]: ");
+  scanf("%i", &value);
+
+  return value;
 }
